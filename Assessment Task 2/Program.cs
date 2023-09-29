d3d6e8bc87d6bcc5ee3e4ebe8cda4bf4f6ded59d
pedri <pedri@LELOUCH>
2023-09-29 21:31:05 +1300

    Add project files.

---
+++ Assessment Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to enter a number
+            Console.Write("Enter a number: ");
+
+            // Read the input number from the user
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Display a message indicating that we are listing factors
+            Console.WriteLine("Factors of " + number +":" );
+
+            // Find and display the factors of the input number
+            for (int i = 1; i <= number; i++)
+            {
+                // Check if 'i' is a factor of the input number
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+        
